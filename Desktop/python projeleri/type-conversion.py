f958200ba0705496af7964c492172192831deb3f
Arda Özgök <ardaozgok7@icloud.com>
2026-09-03 22:40:23 +0300

    Gün 2 pratikleri: Veri tipleri ve tip dönüşümleri

---
+++ Desktop/python projeleri/type-conversion.py
@@ -0,0 +1,108 @@
+"""
+x = input("1.sayı: ")
+y = input("2.sayı: ")
+
+toplam = x + y 
+
+print(toplam)
+
+x = input("1.sayı: ")
+y = input("2.sayı: ")
+
+print(type(x))
+print(type(y))
+
+toplam = int(x) + int(y)
+
+print(toplam)
+
+"""
+
+"""
+
+x = 5    #int
+y = 2.5  #float
+name = "Çınar" # string
+isOnline = True #bool
+
+print(type(x))
+print(type(y))
+print(type(name))
+print(type(isOnline))
+
+"""
+
+# Type Conversion 
+
+#int to float
+
+"""
+
+x= 5
+
+x = float(x)
+print(x)
+print(type(x))
+
+
+"""
+"""
+# float to int 
+ 
+y = 2.5  
+
+y = int(y)
+print(y)
+print(type(y))
+
+x = 2.5
+y = 5
+
+result = x + y
+print(result)
+print(type(result))
+
+
+result = str(x) + str(y)
+print(result)
+print(type(result))
+
+"""
+
+# boll to str
+
+"""
+isOnline = True
+
+isOnline = str(isOnline)
+print(isOnline)
+print(type(isOnline))
+
+"""
+# bool to int
+
+"""
+
+isOnline = True
+isOnline = int(isOnline)
+print(isOnline)
+print(type(isOnline))
+
+isOnline = False
+isOnline = int(isOnline)
+print(isOnline)
+print(type(isOnline))
+
+"""
+
+pi = 3.14
+r  = float(input("yarı çap :"))
+alan = pi * (r  ** 2)
+print(type(alan))
+cevre = 2 * pi * r
+print(type(cevre))
+
+print("alan" , alan)
+print("cevre",cevre)
+
+
---
+++ Desktop/python projeleri/variables-demo.py
@@ -0,0 +1,27 @@
+musteriAdi = "Ali"
+musteriSoyad = "Yılmaz"
+musteriAdSoyad = musteriAdi + " "  + musteriSoyad
+print(musteriAdSoyad)
+musteriCinsiyet = False # Erkek
+musteriTcKimlik = "12345678987"
+musteriDogumYılı = 1989
+musteriAdresi = "Kadıkoy"
+musteriYası = 2026 - musteriDogumYılı
+
+print(musteriYası)
+
+"""
+
+sipariş1 => 100 TL 
+sipariş2 => 1100.5 TL 
+sipariş3 => 356.95 TL 
+
+
+"""
+order1 = 100
+order2 = 1100.5
+order3 = 356.95
+
+total = order1 + order2 + order3
+
+print("Total:",total)
